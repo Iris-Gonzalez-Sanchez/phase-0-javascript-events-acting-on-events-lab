843701309f35293a75d08300bf122d0bf64c7bdf
Iris-Gonzalez-Sanchez <Iris.GonzalezSan@Gmail.com>
2022-05-06 15:58:54 -0400

    commit tree 4728de433436fe161b48287b4c63f3559d927f31

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+  const dodger = document.getElementById("dodger");
+  function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  });
+
+  //event listener for left works, but not for right
+
+  function moveDodgerRight() {
+    const rightNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(rightNumbers, 10);
+    console.log(left);
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });  
